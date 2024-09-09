fe286cdf0374d4d864380539442ddcf8104bcf33
dlwoauddlqslek <ljm5857@gmail.com>
2024-09-09 17:57:32 +0900

    09.09회원가입,sql select 추가 로그설정 주석및 로그파일 삭제

---
+++ src/main/java/web/model/dto/MemberDto.java
@@ -0,0 +1,27 @@
+package web.model.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDto {
+    //09.09 회원Dto
+    private int id;
+    private String userName;
+    private String password;
+    private String name;
+    private String contact;
+    private String email;
+    private String gender;
+    private int age;
+    private String joinDate;
+    private int teamCode;
+    private int purchaseLimitAmount;
+    private int purchaseLimitCount;
+    private int points;
+    private String account;
+}
---
+++ src/main/java/web/model/dto/TeamsDto.java
@@ -0,0 +1,16 @@
+package web.model.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamsDto {
+    //09.09 팀Dto
+    private int teamCode;
+    private String teamName;
+    private String logoImage;
+}
