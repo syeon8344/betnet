d204b3578cbadf0384671e051e7d0b4de7ce568f
tjwlsvy <tjwlsvy1@naver.com>
2024-09-12 18:02:21 +0900

    설문조사 기능구현

--- src/main/java/web/controller/SurveyController.java
+++ src/main/java/web/controller/SurveyController.java
@@ -1,6 +1,7 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import web.service.SurveyService;
 public class SurveyController {
     @Autowired SurveyService surveyService;
 
-    @PostMapping("/save")
+    @GetMapping("/save")
     public int save(SurveyDto surveyDto){
         System.out.println("surveyDto = " + surveyDto);
         return surveyService.save(surveyDto);
