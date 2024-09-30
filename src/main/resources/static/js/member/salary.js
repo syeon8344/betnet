134a206a80c403b3c3c2c3ca01035811b4c18da9
dlwoauddlqslek <ljm5857@gmail.com>
2024-10-01 01:14:09 +0900

    09.30 연봉

--- src/main/resources/static/js/member/salary.js
+++ src/main/resources/static/js/member/salary.js
@@ -9,7 +9,18 @@ function salary(){ console.log('salary()');
         url:"http://127.0.0.1:5000/salary",
         success: (result) => {
             console.log(result);
-            
+            let stat=document.querySelector(".stat");
+            let html=''
+            result.forEach(r =>{
+                html+=`<tr>
+                            <td> ${r.선수명} </td> <td> ${r.타율} </td>    
+                            <td> ${r.타석} </td> <td> ${r.홈런} </td>
+                            <td> ${r.타점} </td> <td> ${r.도루} </td>
+                            <td> ${r.장타율} </td><td> ${r.출루율} </td>
+                            <td> ${r.예상연봉} </td>
+                        </tr>`;
+            })
+            stat.innerHTML=html;
         }
     })
 }
