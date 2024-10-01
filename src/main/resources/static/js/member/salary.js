abee4351dc889831b02433968ebc42bad7db78ce
dlwoauddlqslek <ljm5857@gmail.com>
2024-10-02 04:31:53 +0900

    predict

--- src/main/resources/static/js/member/salary.js
+++ src/main/resources/static/js/member/salary.js
@@ -12,23 +12,30 @@ function salary(){ console.log('salary()');
             let stat=document.querySelector(".player-profile");
             let html=''
             result.forEach(r =>{
-                html+=`<div class="player-image">
-                                       <img src="${r.사진}">
-                                   </div>
-                                   <div class="player-info">
-                                       <p><strong>선수명:</strong>${r.선수명}</p>
-                                       <p><strong>타율:</strong>${r.타율}</p>
-                                       <p><strong>타석:</strong>${r.타석}</p>
-                                       <p><strong>안타:</strong>${r.안타}</p>
-                                       <p><strong>홈런:</strong>${r.홈런}</p>
-                                       <p><strong>타점:</strong>${r.타점}</p>
-                                       <p><strong>도루:</strong>${r.도루}</p>
-                                       <p><strong>볼넷:</strong>${r.볼넷}</p>
-                                       <p><strong>장타율:</strong>${r.장타율}</p>
-                                       <p><strong>출루율:</strong>${r.출루율}</p>
-                                   </div>`;
+                html+=`<div class="flex-container">
+                <div class="player-image">
+                    <img src="${r.사진}" alt="선수 이미지">
+                </div>
+                <div class="player-info">
+                    <p><strong>선수명:</strong> ${r.선수명}</p>
+                    <p><strong>타율:</strong> ${r.타율}</p>
+                    <p><strong>타석:</strong> ${pr.타석}</p>
+                    <p><strong>안타:</strong> ${r.안타}</p>
+                    <p><strong>홈런:</strong> ${r.홈런}</p>
+                </div>
+                <div class="player-stats">
+                    <p><strong>타점:</strong> ${r.타점}</p>
+                    <p><strong>도루:</strong> ${r.도루}</p>
+                    <p><strong>볼넷:</strong> ${r.볼넷}</p>
+                    <p><strong>장타율:</strong> ${r.장타율}</p>
+                    <p><strong>출루율:</strong> ${r.출루율}</p>
+                </div>
+                <div class="player-salary">
+                    예상 연봉: ${r.예상연봉}억 원
+                </div>
+            </div>`
             })
             stat.innerHTML=html;
         }
     })
-}
+ }
